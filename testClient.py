45cdf9fde4c5b387e9511c9fcaa5b2cf5609181e
Ariel Adjadj <arieladjadj1@gmail.com>
2020-05-10 22:11:10 +0300

    Finish

--- testClient.py
+++ testClient.py
@@ -1,4 +1,5 @@
 import socket
+import time 
 
 def printAsciiArt():
     print('''
@@ -22,15 +23,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print("* Connection failed.")
         exit()
     
+    time.sleep(2)
     print("* Connection succeeded \n")
 
     print("* sign up: (username: \"user1\",password: \"1234\", mail:user1@gmail.com )")
+    time.sleep(2)
     x =  '''{username: "user1", password: "1234", mail:user1@gmail.com}'''
     s.sendall(b'\x15\x00\x00\x00;{username: "user1", password: "1234", mail:user1@gmail.com}')
     data = s.recv(1024)
     print("recived: ", repr(data), "\n")
 
     print("* log in: (usernmaee: \"user1\", password: \"1234\")")
+    time.sleep(2)
     x =  '''{username: "user1", password: "1234"}'''
     data  =  b = bytes([20,0x0,0x0,0x0,len(x)]) + x.encode()
     s.sendall(data) 
