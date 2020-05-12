593fbca8a27eaadaff20dd14e63f0e3505293971
Tom Chengich <tomcengich@gmail.com>
2020-05-12 19:02:48 +0300

    Finished IDatabase and MongoDatabase

---
+++ MagshiTriviaServer/IDatabase.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+class IDatabase {
+public:
+	virtual bool doesUserExist(std::string username) = 0;
+	virtual bool doesPasswordMatch(std::string password) = 0;
+	virtual void addNewUser(std::string username, std::string password, std::string email) = 0;
+};
--- MagshiTriviaServer/Main.cpp
+++ MagshiTriviaServer/Main.cpp
@@ -1,14 +1,20 @@
+#define _SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING
 #include <SFML/Network.hpp>
 #include <thread>
 #include <vector>
 #include <nlohmann/json.hpp>
 #include "Server.hpp"
+#include "MongoDatabase.hpp"
+#include <iostream>
 
 int main()
 {
+	/*
 	Server server;
 	server.run();
-	
+	*/
+	MongoDatabase db;
+	std::cout << db.doesPasswordMatch("1234") << std::endl;
 	return 0;
 }
 
