7644784b1096c7a957f5cc911705ac77049fa5ee
Tom Chengich <tomcengich@gmail.com>
2020-05-13 06:50:24 +0300

    Added a default virtual D'TOR

--- MagshiTriviaServer/IDatabase.hpp
+++ MagshiTriviaServer/IDatabase.hpp
@@ -3,6 +3,7 @@
 
 class IDatabase {
 public:
+	virtual ~IDatabase() = default;
 	virtual bool doesUserExist(std::string username) = 0;
 	virtual bool doesPasswordMatch(std::string password) = 0;
 	virtual void addNewUser(std::string username, std::string password, std::string email) = 0;
