741547b60940fa7e30c99361b97b8c614a7be0ad
Tom Chengich <tomcengich@gmail.com>
2020-05-13 06:49:55 +0300

    Implemented LoginManager

--- MagshiTriviaServer/LoginManager.hpp
+++ MagshiTriviaServer/LoginManager.hpp
@@ -1,5 +1,19 @@
 #pragma once
+#include <vector>
+#include "LoggedUser.hpp"
+#include "IDatabase.hpp"
+
 class LoginManager
 {
+public:
+	LoginManager();
+	~LoginManager();
+	void login(std::string username, std::string password);
+	void signup(std::string username, std::string password, std::string email);
+	void logout(std::string username);
+
+private: 
+	std::vector<LoggedUser> m_loggedUsers;
+	IDatabase* m_database;
 };
 
