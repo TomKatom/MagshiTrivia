ca823f0317afeec3accc0ac4d74958951b7fcf21
Tom Chengich <tomcengich@gmail.com>
2020-05-31 19:03:55 +0300

    Added checks to login manager.

--- MagshiTriviaServer/LoginManager.cpp
+++ MagshiTriviaServer/LoginManager.cpp
@@ -11,16 +11,27 @@ LoginManager::~LoginManager() {
 }
 
 void LoginManager::login(std::string username, std::string password) {
-	if (this->m_database->doesPasswordMatch(username, password)) { 
-		this->m_loggedUsers.push_back(LoggedUser(username)); 
+	if (std::find_if(this->m_loggedUsers.begin(), this->m_loggedUsers.end(), [&](auto user) {return user.getUsername() == username; }) == this->m_loggedUsers.end()) {
+
+		if (this->m_database->doesPasswordMatch(username, password)) { 
+			this->m_loggedUsers.push_back(LoggedUser(username)); 
+		}
+		else {
+			throw std::exception("User doesn't exist.");
+		}
 	}
 	else {
-		throw std::exception("User doesn't exist.");
+		throw std::exception("User already logged in.");
 	}
 }
 
 void LoginManager::signup(std::string username, std::string password, std::string email) {
-	this->m_database->addNewUser(username, password, email);
+	if (!this->m_database->doesUserExist(username)) {
+		this->m_database->addNewUser(username, password, email);
+	}
+	else {
+		throw std::exception("User already exists.");
+	}
 }
 
 void LoginManager::logout(std::string username) {
